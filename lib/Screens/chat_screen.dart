8877eccf3201925c32be558388789855d6ce7faa
prince <girjawatiku@gmail.com>
2024-05-01 21:19:02 +0530

    change 2

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -54,6 +54,7 @@ class _chat_screenState extends State<chat_screen> {
   Widget build(BuildContext context) {
     return
       Scaffold(
+        backgroundColor: Color.fromARGB(255, 66, 229, 109),
         appBar: AppBar(
           automaticallyImplyLeading: false,
           title:  Text('Chat Screen'),
